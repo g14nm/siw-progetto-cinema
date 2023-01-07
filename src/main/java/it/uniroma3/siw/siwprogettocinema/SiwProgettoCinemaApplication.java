5226fb73eec972cd09c9cea31557d19b0d836f3e
gianmarco <gianmarcoprisa@hotmail.it>
2023-01-07 17:36:42 +0100

    primo commit

---
+++ src/main/java/it/uniroma3/siw/siwprogettocinema/SiwProgettoCinemaApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwprogettocinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwProgettoCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwProgettoCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/uniroma3/siw/siwprogettocinema/SiwProgettoCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw.siwprogettocinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwProgettoCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
