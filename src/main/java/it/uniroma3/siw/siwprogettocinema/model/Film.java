d2b03dbb16ff4f3aabd81a2513006b0ec8551d74
g14nm <gianmarcoprisa@hotmail.it>
2023-01-23 13:44:37 +0100

    aggiunto cascade remove su film

--- src/main/java/it/uniroma3/siw/siwprogettocinema/model/Film.java
+++ src/main/java/it/uniroma3/siw/siwprogettocinema/model/Film.java
@@ -2,6 +2,7 @@ package it.uniroma3.siw.siwprogettocinema.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -24,7 +25,7 @@ public class Film {
 	
 	private String descrizione;
 	
-	@OneToMany(mappedBy = "film")
+	@OneToMany(mappedBy = "film", cascade = CascadeType.REMOVE)
 	private List<Proiezione> proiezioni;
 	
 	public Long getId() {
