bbe325fb205e1d768cf9027c7ae7a41525358d26
g14nm <gianmarcoprisa@hotmail.it>
2023-02-22 01:03:56 +0100

    aggiunto template films e modifiche css

--- src/main/java/it/uniroma3/siw/siwprogettocinema/authentication/AuthConfiguration.java
+++ src/main/java/it/uniroma3/siw/siwprogettocinema/authentication/AuthConfiguration.java
@@ -41,7 +41,7 @@ public class AuthConfiguration extends WebSecurityConfigurerAdapter {
                 // authorization paragraph: qui definiamo chi può accedere a cosa
                 .authorizeRequests()
                 // chiunque (autenticato o no) può accedere alle pagine index, login, register, ai css e alle immagini
-                .antMatchers(HttpMethod.GET, "/", "/login/**", "/register", "/film", "/css/**", "/images/**", "/locandine/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/", "/login/**", "/register", "/film/**", "/css/**", "/images/**", "/locandine/**").permitAll()
                 // chiunque (autenticato o no) può mandare richieste POST al punto di accesso per login e register 
                 .antMatchers(HttpMethod.POST, "/login", "/register").permitAll()
                 // solo gli utenti autenticati con ruolo DEFAULT possono accedere a risorse con path /user/**
