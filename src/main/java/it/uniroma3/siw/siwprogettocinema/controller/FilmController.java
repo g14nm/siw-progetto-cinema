9f7f55f0d6984930136ad51c3a6c14e39ab525d7
g14nm <gianmarcoprisa@hotmail.it>
2023-02-27 02:12:10 +0100

    aggiunto template per visualizzazione singolo film

--- src/main/java/it/uniroma3/siw/siwprogettocinema/controller/FilmController.java
+++ src/main/java/it/uniroma3/siw/siwprogettocinema/controller/FilmController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import it.uniroma3.siw.siwprogettocinema.service.FilmService;
 
@@ -17,6 +18,12 @@ public class FilmController {
 	public String getFilms(Model model) {
 		model.addAttribute("films", this.filmService.findAll());
 		return "film/films";
+	}
+	
+	@GetMapping("/film/{id}")
+	public String getFilms(@PathVariable Long id, Model model) {
+		model.addAttribute("film", this.filmService.findById(id));
+		return "film/film";
 	}
 	
 }
